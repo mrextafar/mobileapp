fa92a8f967809e8f36633430f27fdc74203ed9c0
Password=1 <student@ws525-527-17.edu.local>
2025-03-15 18:09:20 +0300

    Закончили на создании таблицы

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface AuthorInfo{
+  aName: string
+  aBirth: number
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,7 +12,17 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
-
+  pageName = "Михаил"
+  books = ["Пушкин", "Лермонтов", "Блок"]
+  authors: AuthorInfo[] = [
+    {aName:"Пушкин", aBirth:1799},
+    {aName:"Лермонтов", aBirth:1814},
+    {aName:"Блок", aBirth:1880},
+  ]
   constructor() {}
+
+  btnClick(){
+    this.pageName = this.pageName + "!"
+  }
 
 }
